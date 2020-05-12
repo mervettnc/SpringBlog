977d92824de09a4faaca7848ea7b3218d3b302a9
merve tütüncü <a_merve1996@hotmail.com>
2020-05-12 19:41:17 +0300

    comment

--- SpringBlog/Areas/Admin/dist/js/admin.custom.js
+++ SpringBlog/Areas/Admin/dist/js/admin.custom.js
@@ -1,10 +1,20 @@
 ﻿$(function () {
     bsCustomFileInput.init();
+  
+    $('table[data-table="true"]').each(function (index) {
 
-    $('table[data-table="true"]').DataTable({
-        "responsive": true,
-        "autoWidth": false,
+        var sortOrder = $(this).data("table-sort-order") || "asc";
+        var sortColumn = $(this).data("table-sort-column") || 0;
+
+        $(this).DataTable({
+            "order": [[sortColumn, sortOrder]],
+            "responsive": true,
+            "autoWidth": false,
+        });
+
     });
+
+   
 
     $('textarea[data-snote="true"]').summernote({
         height: 200
@@ -15,9 +25,9 @@
     $("body").on("click", "[data-delete-id]", function (event) {
         event.preventDefault();
         var button = $(this); // Button that triggered the modal
-        var id = button.data('delete-id'); // Extract info from data-* attributes
-        var name = button.data('delete-name'); // Extract info from data-* attributes
-        var action = button.data('delete-action'); // Extract info from data-* attributes
+        var id = button.data('delete-id') // Extract info from data-* attributes
+        var name = button.data('delete-name') // Extract info from data-* attributes
+        var action = button.data('delete-action') // Extract info from data-* attributes
         $("#deleteModalForm").attr("action", action);
         $("#deleteModalItemName").text(name);
         $("#deleteModalItemId").val(id);
